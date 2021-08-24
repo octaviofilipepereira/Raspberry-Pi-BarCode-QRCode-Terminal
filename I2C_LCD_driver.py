e21d61f8695ee7144de62f12a6368eb7863c9d69
Octávio Filipe Pereira <58921861+octaviofilipepereira@users.noreply.github.com>
2021-08-24 13:09:08 +0100

    Update I2C_LCD_driver.py

--- I2C_LCD_driver.py
+++ I2C_LCD_driver.py
@@ -12,6 +12,7 @@
 #
 # Compiled, mashed and generally mutilated 2014-2015 by Denis Pleic
 # Made available under GNU GENERAL PUBLIC LICENSE
+#
 ###
 #
 # Modified Python I2C library for Raspberry Pi
